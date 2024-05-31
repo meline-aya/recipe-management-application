043091e5954b34b6de380912c9942f538be5c520
Méline Soumaire <meline-aya.soumaire@epita.fr>
2024-05-31 10:46:34 +0200

    fix: delete recipe

--- frontend/app.js
+++ frontend/app.js
@@ -2,6 +2,13 @@ const host = '0.0.0.0';
 const port = 3000;
 
 const list_recipes = document.getElementById('recipes');
+const cr = document.getElementById('create');
+const up = document.getElementById('update');
+
+const na = document.getElementById('name');
+const de = document.getElementById('description');
+const ingr = document.getElementById('ingredients');
+const inst = document.getElementById('instructions');
 
 /*
 ** Fetch all recipes and display them.
@@ -38,17 +45,44 @@ function createItem(recipe) {
   del.classList.add('delete')
   item.appendChild(del);
   
+  // let update = document.createElement('button');
+  // update.texttContent = 'Update';
+  // update.classList.add('update')
+  // item.appendChild(update);
+  
   list_recipes.appendChild(item);
 
   let id = recipe['id'];
-  // del.onclick = deleteRecipe(id, del);
-  // del.addEventListener('click', deleteRecipe(id, del));
+  del.addEventListener('click', () => deleteRecipe(id, del));
+  // update.addEventListener('click', () => updateRecipe(id, update));
 }
+
+// async function updateRecipe(id, update){
+//   cr.hidden = false;
+//   up.hidden = true;
+
+//   const response1 = await fetch(`http://${host}:${port}/api/recipes/${id}`, {
+//     method: "GET",
+//   });
+
+//   let resp = response1.json();
+
+//   na.innerHTML = resp.name;
+//   de.innerHTML = resp.description;
+//   ins.innerHTML = resp.instructions;
+//   ing.innerHTML = resp.instructions;
+  
+//   let data = ;
+//   const response2 = await fetch(`http://${host}:${port}/api/recipes/${id}`, {
+//     method: "PUT",
+//     body: data;
+//   });
+        
+//   displayRecipes();
+// }
 
 async function deleteRecipe(id, del){
   del.parentNode.remove();
-
-  console.log(id);
   const response = await fetch(`http://${host}:${port}/api/recipes/${id}`, {
     method: "DELETE",
   });
